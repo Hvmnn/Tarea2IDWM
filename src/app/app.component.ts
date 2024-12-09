0d2ffa0f85d9a6df203290c53adff6ef67aa2103
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-12-08 21:25:52 -0300

    fix: edit html with popup message.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Tarea2IDWM';
+
+  form = {
+    email: '',
+    message: '',
+  };
+
+  isSubmitting = false;
+  showPopup = false;
+
+  onSubmit() {
+    if (!this.form.email || !this.form.message) {
+      alert('Por favor completa todos los campos antes de enviar.');
+      return;
+    }
+    this.isSubmitting = true;
+
+    setTimeout(() => {
+      this.isSubmitting = false;
+      this.showPopup = true;
+      this.resetForm();
+    }, 2000);
+  }
+
+  closePopup() {
+    this.showPopup = false;
+  }
+
+  resetForm() {
+    this.form.email = '';
+    this.form.message = '';
+  }
 
   ngOnInit(): void {
     initFlowbite();
